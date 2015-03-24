54cb0ab09e31cfc716be2df537ca74b96c7ac6a6
manpreetkaur28 <dilpreetrandhawa28@yahoo.com>
2015-03-24 00:22:29 -0400

    task
    webapp

--- js/weather_forcast.js
+++ js/weather_forcast.js
@@ -1,53 +1,54 @@
 // JavaScript Document
-var position= document.getElementById("searchCity");
-var Geo={};
+//var position= document.getElementById("searchCity");
 function getLocation() {
-if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition(success,shoError);
-}
-else {
-alert('Geolocation is not supported');
-}
-}
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
+   if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(success, showError);
     }
+   else{
+            alert('Geolocation is not supported');
+        }
 }
 
 function success(position) {
-      Geo.lat = position.coords.latitude;
-      Geo.lng = position.coords.longitude;
-	  var key = "1165e94c5fbccd61";
-var Weather = "http://api.wunderground.com/api/”+ key +”/forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
-$.ajax({
-url : Weather,
-dataType : "jsonp",
-success : function(data) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+	var key = "1165e94c5fbccd61";
+    var Weather = "http://api.wunderground.com/api/"+ key +"/forecast/geolookup/conditions/q/" + lat + "," + lng + ".json";
+    $.ajax({
+		type: 'GET',
+        url : Weather,
+        dataType : "jsonp",
+        success : function (data) {
 // get all the information
-var location =data['location']['city'];
-var temp = data['current_observation']['temp_f'];
-var img = data['current_observation']['icon_url'];
-var desc = data['current_observation']['weather'];
-var wind = data['current_observation']['wind_string'];
-}
-});
+            var location = data['location']['city'];
+            var temp = data['current_observation']['temp_f'];
+            var img = data['current_observation']['icon_url'];
+            var desc = data['current_observation']['weather'];
+            var wind = data['current_observation']['wind_string'];
+         }
+     });
 //setting the spans to the correct parameters
-$('#location').html(location);
-$('#temp').html(temp);
-$('#desc').html(desc);
-$('#wind').html(wind);
+    $('#location').html(location);
+    $('#temp').html(temp);
+    $('#desc').html(desc);
+    $('#wind').html(wind);
 //filling the image src attribute with the image url
-$('#img').attr('src', img);
+    $('#img').attr('src', img);
+}
+
+function showError(error) {
+    switch(error.code) {
+    case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.");
+        break;
+    case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+    case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+    case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+    }
 }
